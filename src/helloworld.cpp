3787f011f79b38dd85ed379816137d006e22cc5e
Borgioli <borgioli27@gmail.com>
2023-09-29 12:11:58 -0500

    .
    .

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -35,24 +35,7 @@ int __cdecl main(int argc, char **argv)
 {
     ros::init(argc, argv, "pub_manus_glove");
     ros::NodeHandle n;
-    ros::Publisher tf_pub = n.advertise<geometry_msgs::Transform>("/manus/tf", 1000);
     ros::Publisher glove_pub  = n.advertise<sitl_manus::glove>("/manus/cp", 1000);
-    // ros::Publisher glove_node2_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node2/cp", 1000);
-    // ros::Publisher glove_node3_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node3/cp", 1000);
-    // ros::Publisher glove_node4_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node4/cp", 1000);
-    // ros::Publisher glove_node5_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node5/cp", 1000);
-    // ros::Publisher glove_node6_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node6/cp", 1000);
-    // ros::Publisher glove_node7_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node7/cp", 1000);
-    // ros::Publisher glove_node8_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node8/cp", 1000);
-    // ros::Publisher glove_node9_pub  = n.advertise<geometry_msgs::TransformStamped>("/manus/node9/cp", 1000);
-    // ros::Publisher glove_node10_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node10/cp", 1000);
-    // ros::Publisher glove_node11_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node11/cp", 1000);
-    // ros::Publisher glove_node12_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node12/cp", 1000);
-    // ros::Publisher glove_node13_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node13/cp", 1000);
-    // ros::Publisher glove_node14_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node14/cp", 1000);
-    // ros::Publisher glove_node15_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node15/cp", 1000);
-    // ros::Publisher glove_node16_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node16/cp", 1000);
-    // ros::Publisher glove_node17_pub = n.advertise<geometry_msgs::TransformStamped>("/manus/node17/cp", 1000);
 
     ros::Rate loop_rate(70);
 
@@ -119,8 +102,7 @@ int __cdecl main(int argc, char **argv)
 
 
     // Allocate memory for the vector data
-    std::vector<float> receivedFloatVector(119);
-    size_t vectorSize=119;
+    size_t vectorSize=21*7;
     geometry_msgs::Transform tf;
     sitl_manus::glove msg;
 
@@ -133,6 +115,7 @@ int __cdecl main(int argc, char **argv)
         // Check for errors in iRecvResult here...
 
         // Receive the vector data
+        std::vector<float> receivedFloatVector(vectorSize);
         iResult = recv(ConnectSocket, (char*)receivedFloatVector.data(), sizeof(float) * vectorSize, 0);
 
         //receive rotx
@@ -142,49 +125,49 @@ int __cdecl main(int argc, char **argv)
             // printf("Received Rotx %f\n", receivedRotX);
             // Print the received data
             // publishing(ros::Publisher pub, geometry_msgs::TransformStamped msg, int begin)
-            // tf = set_msg(tf,receivedFloatVector,0);
-            tf.translation.x = receivedFloatVector[0];
-            tf.translation.y = receivedFloatVector[0+1];
-            tf.translation.z = receivedFloatVector[0+2];
-            tf.rotation.x    = receivedFloatVector[0+3];
-            tf.rotation.y    = receivedFloatVector[0+4];
-            tf.rotation.z    = receivedFloatVector[0+5];
-            tf.rotation.w    = receivedFloatVector[0+6];
+            tf = set_msg(tf,receivedFloatVector,0);
             msg.transform1 = tf;
-            // tf = set_msg(tf,receivedFloatVector,7);
-            // msg.transform2 = tf;
-            // tf = set_msg(tf,receivedFloatVector,14);
-            // msg.transform3 = tf;
-            // tf = set_msg(tf,receivedFloatVector,21);
-            // msg.transform4 = tf;
-            // tf = set_msg(tf,receivedFloatVector,28);
-            // msg.transform5 = tf;
-            // tf = set_msg(tf,receivedFloatVector,35);
-            // msg.transform6 = tf;
-            // tf = set_msg(tf,receivedFloatVector,42);
-            // msg.transform7 = tf;
-            // tf = set_msg(tf,receivedFloatVector,49);
-            // msg.transform8 = tf;
-            // tf = set_msg(tf,receivedFloatVector,56);
-            // msg.transform9 = tf;
-            // tf = set_msg(tf,receivedFloatVector,63);
-            // msg.transform10 = tf;
-            // tf = set_msg(tf,receivedFloatVector,70);
-            // msg.transform11 = tf;
-            // tf = set_msg(tf,receivedFloatVector,77);
-            // msg.transform12 = tf;
-            // tf = set_msg(tf,receivedFloatVector,84);
-            // msg.transform13 = tf;
-            // tf = set_msg(tf,receivedFloatVector,91);
-            // msg.transform14 = tf;
-            // tf = set_msg(tf,receivedFloatVector,98);
-            // msg.transform15 = tf;
-            // tf = set_msg(tf,receivedFloatVector,105);
-            // msg.transform16 = tf;
-            // tf = set_msg(tf,receivedFloatVector,112);
-            // msg.transform17 = tf;
+            tf = set_msg(tf,receivedFloatVector,7);
+            msg.transform2 = tf;
+            tf = set_msg(tf,receivedFloatVector,14);
+            msg.transform3 = tf;
+            tf = set_msg(tf,receivedFloatVector,21);
+            msg.transform4 = tf;
+            tf = set_msg(tf,receivedFloatVector,28);
+            msg.transform5 = tf;
+            tf = set_msg(tf,receivedFloatVector,35);
+            msg.transform6 = tf;
+            tf = set_msg(tf,receivedFloatVector,42);
+            msg.transform7 = tf;
+            tf = set_msg(tf,receivedFloatVector,49);
+            msg.transform8 = tf;
+            tf = set_msg(tf,receivedFloatVector,56);
+            msg.transform9 = tf;
+            tf = set_msg(tf,receivedFloatVector,63);
+            msg.transform10 = tf;
+            tf = set_msg(tf,receivedFloatVector,70);
+            msg.transform11 = tf;
+            tf = set_msg(tf,receivedFloatVector,77);
+            msg.transform12 = tf;
+            tf = set_msg(tf,receivedFloatVector,84);
+            msg.transform13 = tf;
+            tf = set_msg(tf,receivedFloatVector,91);
+            msg.transform14 = tf;
+            tf = set_msg(tf,receivedFloatVector,98);
+            msg.transform15 = tf;
+            tf = set_msg(tf,receivedFloatVector,105);
+            msg.transform16 = tf;
+            tf = set_msg(tf,receivedFloatVector,112);
+            msg.transform17 = tf;
+            tf = set_msg(tf,receivedFloatVector,112);
+            msg.transform18 = tf;
+            tf = set_msg(tf,receivedFloatVector,112);
+            msg.transform19 = tf;
+            tf = set_msg(tf,receivedFloatVector,112);
+            msg.transform20 = tf;
+            tf = set_msg(tf,receivedFloatVector,112);
+            msg.transform21 = tf;
 
-            tf_pub.publish(tf);
             glove_pub.publish(msg);
 
             printf("Received Float Vector:\n");
@@ -196,7 +179,7 @@ int __cdecl main(int argc, char **argv)
             printf("Connection closed\n");
         else
             printf("recv failed with error: %d\n", WSAGetLastError());
-
+        receivedFloatVector.clear();
         ros::spinOnce();
         loop_rate.sleep();
     } while(ros::ok());
